0fedeeaa031a605b7aa4c9deefeeb59464f0abe5
wallison <wallison@notebook>
2017-09-23 16:45:11 -0300

    - Projeto inicial

---
+++ hexapod_rna_ag/src/main/java/br/com/tcc/hexapod/hexapod_rna_ag/App.java
@@ -0,0 +1,13 @@
+package br.com.tcc.hexapod.hexapod_rna_ag;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
